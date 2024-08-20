1664644d8e071375ce6a8ab8e6fda239d867f8d8
Dylan Tin <dylant711@gmail.com>
2024-08-20 16:07:10 -0400

    finished elements lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+// <h1></h1>
+newHeader.setAttribute('id', 'victory');
+// <h1 id='victory'></h1>
+
+newHeader.textContent = "Stan is the champion!";
+// <h1 id='victory> Stan is the champion </h1>
